770ff837627023d2ca79860f71993b249aec45ba
mfmehdi <mfmehdi2@gmail.com>
2019-11-08 15:08:34 +0000

    css main and nav

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 
-$("#btn-tools").on("click",listingTools)
+$(".divs-main").hide()
+
+$("#btn-tools").on("click",()=>{
+$(".staff").slideUp("slow");
+$(".movement").slideUp("slow");
+$(".tools").slideDown("slow");
+	listingTools
+
+});
+
+
+
+$("#btn-staff").on("click",()=>{
+$(".tools").slideUp("slow");
+$(".movement").slideUp("slow");
+$(".staff").slideDown("slow");
+	listingStaff
+});
+
+
+$("#btn-movement").on("click",()=>{
+$(".tools").slideUp("slow");
+$(".staff").slideUp("slow");
+$(".movement").slideDown("slow");
+	listingStaff
+});
